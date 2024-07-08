bf68de3ec2822f71187512084e1d5862cdbb9631
Nathan King <nathanking3590@gmail.com>
2024-07-08 11:23:05 +0100

    update experience data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,7 +5,11 @@ const data: Experience[] = [
     id: 1,
     title: 'Software Engineering Teacher & Teacher Coordinator',
     company: 'Boolean UK',
-    description:  'Responsible for designing and delivering a 6 month, full-time software development course. The curriculum takes career changers through a full web stack including JS, JS DOM, HTML/CSS, React, Express, relational databases and PrismaJS.\n\nOther responsibilities include coordinating teacher schedules, tracking new curriculum development and acting as a team lead for cohorts of 30 people taking part in a month-long agile development simulation.',
+    description: `&#8226; Designed and delivered a 6-month full-stack software engineering curriculum.\n\n
+    &#8226; Led team development simulations for 30-40 junior developers, delivering production-grade applications.\n\n
+    &#8226; Conducted pair programming and code review sessions consistently.\n\n
+    &#8226; Produced detailed technical documentation.\n\n
+    &#8226; Implemented unit test suites for practical exercises to validate coursework completion.`,
     dateFrom: 'August 2021',
     dateTo: 'Present',
     tech: [
@@ -23,9 +27,10 @@ const data: Experience[] = [
   },
   {
     id: 2,
-    title: 'Freelance Developer & Sabbatical',
+    title: 'Full-stack Developer',
     company: 'Self-employed',
-    description:  'Contracted by social media entrepreneurs to set up infrastructure for acquiring clients. This mostly involved aggregating data from platforms such as ClickFunnels, Teachable, Stripe and ActiveCampaign into a dashboard style React/Electron app for reporting & charting purposes.\n\nI took a sabbatical between April - August 2021 to avoid burn-out.',
+    description: `&#8226; Created a dashboard-style React/Electron application for social media entrepreneurs to report and visualize data.\n\n
+    &#8226; Aggregated data from various platforms such as ClickFunnels, Teachable and Stripe.`,
     dateFrom: 'November 2019',
     dateTo: 'August 2021',
     tech: [
@@ -40,9 +45,11 @@ const data: Experience[] = [
   },
   {
     id: 3,
-    title: 'Full Stack Developer',
+    title: 'Full-stack Developer',
     company: 'Horizen (formerly ZenCash)',
-    description:  'Primary developer of the flagship cryptocurrency wallet “Sphere by Horizen”. This was my first exposure to Electron and React (class based). I also developed a market maker bot in Java designed to automate trading between exchange platforms.',
+    description: `&#8226; Developed the flagship cryptocurrency wallet 'Sphere by Horizen' using Electron & React.\n\n
+    &#8226; Implemented a substantial performance enhancement for unspent transaction handling.\n\n
+    &#8226; Designed and implemented a Java bot to automate trading across exchange platforms.`,
     dateFrom: 'April 2018',
     dateTo: 'November 2019',
     tech: [
@@ -58,7 +65,10 @@ const data: Experience[] = [
     id: 4,
     title: 'Backend Developer',
     company: 'Opia',
-    description:  'Developer on an in-house internal system that processed CapitalOne pre-paid reward cards for customers making large purchases. Involved a lot of test-driven development, object-oriented programming, batch data processing and API integrations with payment providers.',
+    description: `&#8226; Developed REST API systems to generate and report on CapitalOne pre-paid reward cards.\n\n
+    &#8226; Utilized TDD, OOP, batch data processing, and integrated payment provider APIs.\n\n
+    &#8226; Implemented CQRS architecture to scale query and command bus systems separately.\n\n
+    &#8226; Designed event sourcing systems to maintain immutable, re-playable application states.`,
     dateFrom: 'June 2016',
     dateTo: 'March 2018',
     tech: [
@@ -74,9 +84,10 @@ const data: Experience[] = [
   },
   {
     id: 5,
-    title: 'Backend Developer & App Maintenance',
+    title: 'Backend Developer',
     company: 'Accenture',
-    description:  'Worked on middleware systems designed to connect various NHS systems through a centralised data stream, primarily x-ray image processing servers.',
+    description: `&#8226; Developed middleware systems for NHS data streams and x-ray image processing.\n\n
+    &#8226; Created a dashboard using vanilla PHP, HTML & CSS to report on NHS server states.`,
     dateFrom: 'January 2015',
     dateTo: 'April 2016',
     tech: [
@@ -95,7 +106,8 @@ const data: Experience[] = [
     id: 6,
     title: 'Software Developer (Entry-level)',
     company: 'Strategic Corrosion Management',
-    description:  'Worked on a small team developing pipeline corrosion reporting software for the oil & gas industry. Clients included Maersk, Centrica and Shell.',
+    description: `&#8226; Developed pipeline corrosion reporting software for clients such as Maersk, Centrica, and Shell.\n\n
+    &#8226; Generated useful reports from a denormalised, poorly implemented legacy database by writing complex SQL statements.`,
     dateFrom: 'August 2013',
     dateTo: 'January 2015',
     tech: [
